62dd5d7b7da61f8b36a19aed5d32a96b7c32d6ac
Daniyal <daniyalishfaq10@gmail.com>
2023-04-21 07:21:27 +0500

    created a simple window

--- rolling_ball.cpp
+++ rolling_ball.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <GLUT/glut.h>
 using namespace std;
 
-int main()
+// Program to create an empty Widdow
+void init()
 {
-    cout << "Hello, world!" << endl;
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(1000, 200);
+    glutCreateWindow("Rolling Ball");
+}
+
+void display()
+{
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    // gluOrtho2D(0.0,100.0,0,100.0);
+    glFlush();
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv); // Line A
+    init();                // Line B
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
 }
